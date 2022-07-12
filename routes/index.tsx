71fe2691dd99287235436c8fc348ddf72fdad6cb
Mike <mike@failsafedesign.com>
2022-07-12 00:48:41 -0500

    add head

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,6 +3,17 @@ import { h } from "preact";
 import { tw } from "@twind";
 import Counter from "../islands/Counter.tsx";
 
+export const Head = () => {
+    <head>
+        <title>Index page</title>
+        <meta
+            name="description"
+            content="This is an example of a
+meta description. This will often show up in search results."
+        />
+    </head>;
+};
+
 export default function Home() {
     return (
         <div class={tw`p-4 mx-auto max-w-screen-md`}>
