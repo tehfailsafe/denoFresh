205970ca4cbc71562dd3bcd29c5da7d2fc2b4c56
Mike <mike@failsafedesign.com>
2022-07-12 00:43:21 -0500

    refresh

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,7 +5,6 @@
 import * as $0 from "./routes/about.tsx";
 import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/index.tsx";
-import * as $3 from "./routes/test.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -13,7 +12,6 @@ const manifest = {
     "./routes/about.tsx": $0,
     "./routes/api/joke.ts": $1,
     "./routes/index.tsx": $2,
-    "./routes/test.tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
